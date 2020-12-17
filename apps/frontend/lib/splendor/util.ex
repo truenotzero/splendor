6d22e7646a8c7ba43cbb483561acc69ef9f80b59
truenotzero <truenotzero@gmail.com>
2020-12-17 15:58:44 +0200

    Refactor as umbrella! Still have to fix names

--- apps/frontend/lib/splendor/util.ex
+++ apps/frontend/lib/splendor/util.ex
@@ -5,12 +5,12 @@ defmodule Splendor.Util do
 
   @doc """
   Fetches the config for the current module.
-  
-  For example, if the current module is `Foo`, then this is equivalent to `Application.fetch_env!(:splendor, Foo)`
+
+  For example, if the current module is `Foo`, then this is equivalent to `Application.fetch_env!(:frontend, Foo)`
   """
   defmacro fetch_module_config!() do
     quote do
-      Application.fetch_env!(:splendor, __MODULE__)
+      Application.fetch_env!(:frontend, __MODULE__)
     end
   end
 end
--- config/config.exs
+++ config/config.exs
@@ -1,10 +1,10 @@
 import Config
 
-config :splendor, Splendor.Session,
+config :frontend, Splendor.Session,
   major: 95,
   minor: "1",
   locale: 8
 
 
-config :splendor, Splendor.CustomOFBCipher,
+config :frontend, Splendor.CustomOFBCipher,
   key: <<0x13, 0x08, 0x06, 0xB4, 0x1B, 0x0F, 0x33, 0x52>>
