63cc9652ab3b82e17b3e9df1d9e5e012a338285d
truenotzero <truenotzero@gmail.com>
2020-12-20 09:45:48 +0200

    Basic API
    Laid out the foundation of the presence server

--- apps/presence/lib/splendor/presence.ex
+++ apps/presence/lib/splendor/presence.ex
@@ -1,18 +1,29 @@
 defmodule Splendor.Presence do
   @moduledoc """
-  Documentation for `Splendor.Presence`.
+  The presence core is used for tracking online players
   """
+
+  @typedoc """
+  Tracks the current stage the player is connected to
+  Could be `login`, `game` or `shop`
+  """
+  @spec stage :: atom()
+  @spec info :: any()
+  @spec t :: :offline | {:online, stage(), info()}
 
   @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Splendor.Presence.hello()
-      :world
-
+  Locates a player by their account id
   """
-  def hello do
-    :world
+  @spec locate_account(non_neg_integer()) :: t()
+  def locate_account(account_id) do
+    :todo
+  end
+
+  @doc """
+  Locates a player by their ip address
+  """
+  @spec locate_ip(:inet.ip_address()) :: t()
+  def locate_ip(ip_addr) do
+    :todo
   end
 end
